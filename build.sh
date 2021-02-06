159408663c5dbe35b3789eefa4a53f6ca5205e6d
Adam Judge <adamdavidjudgee@gmail.com>
2021-02-06 15:17:17 -0500

    Add 'clean' and 'disk' commands to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 
 VERSION=0.0.1
 
+if [ "$1" == "clean" ]; then
+    rm *.bin
+    rm kernel/build.asm
+    rm -f osalpha.img
+    exit
+fi
+
 echo "version:" > kernel/build.asm
 echo "    db \"Kernel version: $VERSION\", 0xD, 0xA" >> kernel/build.asm
 echo "    db \"Compiled: $(date)\", 0xD, 0xA, 0" >> kernel/build.asm
@@ -12,3 +19,15 @@ cd kernel
 nasm main.asm -o kernel.bin
 cd ..
 mv kernel/kernel.bin .
+
+if [ "$1" == "disk" ]; then
+    mkfs.msdos -C osalpha.img 1440
+    dd if=bootload.bin of=osalpha.img conv=notrunc
+    sudo mkdir /mnt/floppy
+    sudo mount -o loop osalpha.img /mnt/floppy
+
+    sudo cp kernel.bin /mnt/floppy/
+
+    sudo umount /mnt/floppy
+    sudo rm -r /mnt/floppy
+fi
